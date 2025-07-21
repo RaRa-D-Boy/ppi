7846ee6d454ec5a752ee560964a8fcf9ad0a9997
Gideon Fiawornu <gfiawornu@persol.com>
2025-07-21 18:59:45 +0100

    update

--- ppi-main/app/page.tsx
+++ ppi-main/app/page.tsx
@@ -13,7 +13,6 @@ export default function Page() {
       <HomePage />
       <OurStory />
       <WhatWeDoing />
-      {/* <CardGrid/> */}
       <ExecutingStrategy />
       <EconomicRationalePage />
       <WorkForce />
